eb841d8bdd072c39de235ec7c9469fe25004ff89
Oliver Lukesch <oliver.lukesch@gmail.com>
2016-11-19 20:09:24 +0100

    holy moly

--- src/Bounce.js
+++ src/Bounce.js
@@ -8,12 +8,15 @@ let defaults = {
   },
 
   private: {
-    isActive: false,
     startPosition: 0,
     currentPosition: 0,
     targetPosition: 0,
     animateTime: 0,
     startTime: 0
+  },
+
+  state: {
+    isActive: false
   }
 };
 
@@ -29,6 +32,7 @@ export default class Bounce {
   constructor(config) {
     this._config = fUtils.cloneDeep(defaults.config);
     this._private = fUtils.cloneDeep(defaults.private);
+    this._state = fUtils.cloneDeep(defaults.state);
 
     if (config) fUtils.mergeDeep(this._config, config);
 
@@ -45,8 +49,8 @@ export default class Bounce {
   startBounce(startPosition, targetPosition, animateTime) {
     cancelAnimationFrame(this._private.currentFrame);
 
-    if (!this._private.isActive) this.dispatchEvent(new Event(events.bounceStart));
-    this._private.isActive = true;
+    if (!this._state.isActive) this.dispatchEvent(new Event(events.bounceStart));
+    this._state.isActive = true;
 
     this._private.startPosition = startPosition;
     this._private.currentPosition = startPosition;
@@ -59,8 +63,8 @@ export default class Bounce {
 
 
   stopBounce() {
-    if (this._private.isActive) {
-      this._private.isActive = false;
+    if (this._state.isActive) {
+      this._state.isActive = false;
       this.dispatchEvent(new Event(events.bounceEnd));
     }
 
@@ -74,7 +78,7 @@ export default class Bounce {
   _runBounce() {
     let shouldBounceEnd = false;
 
-    if (this._private.isActive) {
+    if (this._state.isActive) {
       let timePassed = Date.now() - this._private.startTime;
 
       // we test the passed time instead of the position as:
@@ -96,9 +100,9 @@ export default class Bounce {
       this.dispatchEvent(new Event(events.bounceBy), this._private.currentPosition);
     }
 
-    // check for this._private.isActive in addition to shouldBounceEnd as a fail-safe in case the
+    // check for this._state.isActive in addition to shouldBounceEnd as a fail-safe in case the
     // _runBounce() keeps on executing even after the bounce should have ended
-    if (!shouldBounceEnd && this._private.isActive) {
+    if (!shouldBounceEnd && this._state.isActive) {
       this._private.currentFrame = requestAnimationFrame(this._private.boundRunBounce);
     }
     else {
